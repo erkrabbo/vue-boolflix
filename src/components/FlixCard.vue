ef79a83e956398470690957933f24b8b4a6249ab
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-30 12:02:21 +0200

    align

--- src/components/FlixCard.vue
+++ src/components/FlixCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col">
-    <div class="card h-100">
+    <div class="card align-items-center h-100">
       <h2 v-if="dataFilm.name != dataFilm.original_name">{{dataFilm.name}}</h2>
       <h2>{{dataFilm.original_name}}</h2>
       <h2 v-if="dataFilm.title != dataFilm.original_title">{{dataFilm.title}}</h2>
@@ -11,8 +11,8 @@
         <span v-for="(stars, index) in averageStars(dataFilm.vote_average)" :key="index">s</span>
         <span v-for="(stars,index) in (5 - averageStars(dataFilm.vote_average))" :key="index">v</span>
       </div>
-      <img v-if="dataFilm.poster_path != null" :src="'https://image.tmdb.org/t/p/original/' + dataFilm.poster_path" :alt="dataFilm.original_title">
-      <img v-else src="https://wellesleysocietyofartists.org/wp-content/uploads/2015/11/image-not-found.jpg" alt="image not found">
+      <img v-if="dataFilm.poster_path != null" class="card-img" :src="'https://image.tmdb.org/t/p/original/' + dataFilm.poster_path" :alt="dataFilm.original_title">
+      <img v-else class="card-img" src="https://wellesleysocietyofartists.org/wp-content/uploads/2015/11/image-not-found.jpg" alt="image not found">
     </div>
   </div>
 </template>
